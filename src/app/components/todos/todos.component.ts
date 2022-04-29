f157d088713472c37c71c20d5782811f24a4f343
TheJhonni <davideconi91@gmail.com>
2022-04-29 10:07:57 +0200

    1

---
+++ src/app/components/todos/todos.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from './../../models/Todo';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css'],
+})
+export class TodosComponent implements OnInit {
+  todos: Todo[];
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.todos = [
+      {
+        content: 'First todo',
+        completed: false,
+      },
+      {
+        content: 'Second todo',
+        completed: true,
+      },
+    ];
+  }
+}
